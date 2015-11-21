6227ee2658c87ff413d17a6e6341f8fdb39cbcd8
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-11-20 17:11:27 -0800

    Do some housekeeping

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var app = require('app')
-var BrowserWindow = require('browser-window')
+var app = require('app');
+var BrowserWindow = require('browser-window');
 
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
     width: 800,
     height: 600
-  })
-  mainWindow.loadUrl('file://' + __dirname + '/index.html')
-})
+  });
+
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+});
