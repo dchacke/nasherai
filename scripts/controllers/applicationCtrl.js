f5168eddb0d3a4db96f4637688a0de8dfeed719e
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-11-20 20:56:34 -0800

    For now, make preview read-only

--- scripts/controllers/applicationCtrl.js
+++ scripts/controllers/applicationCtrl.js
@@ -1,19 +1,14 @@
 angular.module('nasherai')
   .controller('ApplicationCtrl', function ($scope) {
     $scope.watchables = {
-      code: 'Code goes hereee!',
-      preview: 'Preview'
+      code: '',
+      preview: ''
     };
 
     $scope.$watch(function () {
       return $scope.watchables.code;
     }, function (code) {
-      console.log('code', code);
-    }, true);
-
-    $scope.$watch(function () {
-      return $scope.watchables.preview;
-    }, function (preview) {
-      console.log('preview', preview);
+      code = String(code).replace(/<[^>]+>|&nbsp;/gm, '');
+      eval(code);
     }, true);
   });
